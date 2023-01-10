bab75f780685695271020be71f1d5bc446c219c3
bolero2 <sheocjf1025@gmail.com>
2023-01-10 15:16:48 +0900

    feat: writing dataset/custom.py for dataloader in tf2

--- datasets/custom.py
+++ datasets/custom.py
@@ -4,10 +4,14 @@ import numpy as np
 import os
 from PIL import Image
 import cv2
+import random
 
 
 IMAGE_EXT = 'jpg'
 LABEL_EXT = 'png'
+
+y_k_size = 6
+x_k_size = 6
 
 class CustomDataset(Sequence):
     def __init__(self, config,
@@ -22,13 +26,20 @@ class CustomDataset(Sequence):
                  reshape_size=(640, 480),
                  scale_factor=16,
                  mean=[0.485, 0.456, 0.406], 
-                 std=[0.229, 0.224, 0.225]
+                 std=[0.229, 0.224, 0.225],
+                 bd_dilate_size=4,
+                 mode=''
                 ):
+
+        assert mode in ['train', 'valid', 'test']
+        self.mode = mode
 
         self.config = config
         self.datapack = datapack
-        self.image_prefix = os.path.join(config.DATASET.ROOT, config.DATASET.IMAGES)
-        self.label_prefix = os.path.join(config.DATASET.ROOT, config.DATASET.ANNOTATIONS)
+
+        self.image_prefix = os.path.join(config.DATASET.ROOT, self.mode, config.DATASET.IMAGES)
+        self.label_prefix = os.path.join(config.DATASET.ROOT, self.mode, config.DATASET.ANNOTATIONS)
+
 
         self.multi_scale = multi_scale
         self.flip = flip
@@ -41,6 +52,7 @@ class CustomDataset(Sequence):
 
         self.mean = mean
         self.std = std
+        self.bd_dilate_size = bd_dilate_size
 
         self.batch_size = batch_size
         self.shuffle = shuffle
@@ -71,7 +83,14 @@ class CustomDataset(Sequence):
             y = y.resize(self.reshape_size)
             y = np.array(y)
 
-        return None
+        image, label, edge = self.gen_sample(x, y, 
+                                self.multi_scale, self.flip, edge_pad=False,
+                                edge_size=self.bd_dilate_size)
+        _image = image.copy()
+        _label = label.copy()
+        _edge = edge.copy()
+
+        return _image, _label, _edge, np.array(size), [self.datapack[x] for x in indices]
 
     def on_epoch_end(self):
         self.indices = np.arange(len(self.datapack))
@@ -84,19 +103,17 @@ class CustomDataset(Sequence):
         edge = cv2.Canny(label, 0.1, 0.2)
         kernel = np.ones((edge_size, edge_size), np.uint8)
         if edge_pad:
-            edge = edge[y_k_size:-y_k_size, x_k_size:-x_k_size]
-            edge = np.pad(edge, ((y_k_size,y_k_size),(x_k_size,x_k_size)), mode='constant')
-        edge = (cv2.dilate(edge, kernel, iterations=1)>50)*1.0
+            edge = edge[y_k_size:-1 * y_k_size, x_k_size:-1 * x_k_size]
+            edge = np.pad(edge, ((y_k_size, y_k_size),(x_k_size, x_k_size)), mode='constant')
+        edge = (cv2.dilate(edge, kernel, iterations=1) > 50) * 1.0
         
         if multi_scale:
             rand_scale = 0.5 + random.randint(0, self.scale_factor) / 10.0
             image, label, edge = self.multi_scale_aug(image, label, edge,
                                                 rand_scale=rand_scale)
 
-        image = self.input_transform(image, city=city)
+        image = self.input_transform(image)
         label = self.label_transform(label)
-        
-
         image = image.transpose((2, 0, 1))
 
         if is_flip:
@@ -104,5 +121,71 @@ class CustomDataset(Sequence):
             image = image[:, :, ::flip]
             label = label[:, ::flip]
             edge = edge[:, ::flip]
+
+        return image, label, edge
+
+    def input_transform(self, image):
+        image = image.astype(np.float32)
+        image = image / 255.0
+        image -= self.mean
+        image /= self.std
+        return image
+
+    def label_transform(self, label):
+        return np.array(label).astype(np.uint8)
+
+    def pad_image(self, image, h, w, size, padvalue):
+        pad_image = image.copy()
+        pad_h = max(size[0] - h, 0)
+        pad_w = max(size[1] - w, 0)
+        if pad_h > 0 or pad_w > 0:
+            pad_image = cv2.copyMakeBorder(image, 0, pad_h, 0,
+                                           pad_w, cv2.BORDER_CONSTANT,
+                                           value=padvalue)
+
+        return pad_image
+
+    def rand_crop(self, image, label, edge):
+        h, w = image.shape[:-1]
+        image = self.pad_image(image, h, w, self.crop_size,
+                               (0.0, 0.0, 0.0))
+        label = self.pad_image(label, h, w, self.crop_size,
+                               (self.ignore_label,))
+        edge = self.pad_image(edge, h, w, self.crop_size,
+                               (0.0,))
+
+        new_h, new_w = label.shape
+        x = random.randint(0, new_w - self.crop_size[1])
+        y = random.randint(0, new_h - self.crop_size[0])
+        image = image[y:y + self.crop_size[0], x:x + self.crop_size[1]]
+        label = label[y:y + self.crop_size[0], x:x + self.crop_size[1]]
+        edge = edge[y:y + self.crop_size[0], x:x + self.crop_size[1]]
+
+        return image, label, edge
+
+    def multi_scale_aug(self, image, label=None, edge=None,
+                        rand_scale=1, rand_crop=True):
+        long_size = np.int(self.base_size * rand_scale + 0.5)
+        h, w = image.shape[:2]
+        if h > w:
+            new_h = long_size
+            new_w = np.int(w * long_size / h + 0.5)
+        else:
+            new_w = long_size
+            new_h = np.int(h * long_size / w + 0.5)
+
+        image = cv2.resize(image, (new_w, new_h),
+                           interpolation=cv2.INTER_LINEAR)
+        if label is not None:
+            label = cv2.resize(label, (new_w, new_h),
+                               interpolation=cv2.INTER_NEAREST)
+            if edge is not None:
+                edge = cv2.resize(edge, (new_w, new_h),
+                                   interpolation=cv2.INTER_NEAREST)
+        else:
+            return image
+
+        if rand_crop:
+            image, label, edge = self.rand_crop(image, label, edge)
 
         return image, label, edge
